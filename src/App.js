bad2d28e5f2fbb69d13456fd2775735452fac711
halynaburdiian <verdamtion@gmail.com>
2019-07-17 15:55:07 +0200

    Finishing app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import DogsList from './componets/DogsList'
 import DogsListContainer from './componets/DogsListContainer';
 import { Route } from 'react-router-dom'
-import DogBreedImages from './componets/DogBreedImages'
-import { Link } from 'react-router-dom'
+
 import DogBreedImagesContainer from './componets/DogBreedImagesContainer'
 
 function App() {
--- src/componets/DogBreedImages.js
+++ src/componets/DogBreedImages.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+
 
 export default function DogBreedImages (props) {
   return (
@@ -10,9 +10,8 @@ export default function DogBreedImages (props) {
       {/* This page will show images of the { props.match.params.breed } breed. */}
        
       <Link to="/">Go back to the index</Link>
-    <div>
-        { images && images.map(url => <img src={ url } alt="Dog" />) }
-        { !images && 'Loading...' }
+    <div>{props.images && props.images.map(url => <img src={ url } alt="Dog" />) }
+        { !props.images && 'Loading...' }
     </div>
       
     </div>
